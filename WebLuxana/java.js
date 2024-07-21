fb402ee3e91370da5fb18a6267c6a2cc4014f6a3
LuuxDD <luisinabender@hotmail.com>
2024-07-21 20:53:12 -0300

    first commit

---
+++ WebLuxana/java.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var parrafoInicio = document.querySelector('.parrafo_inicio');
+    var posicionDesdeArriba = parrafoInicio.getBoundingClientRect().top;
+    var tamañoDePantalla = window.innerHeight / 1.3; // Ajusta este valor según necesites
+
+    if (posicionDesdeArriba < tamañoDePantalla) {
+        parrafoInicio.classList.add('animar-scroll');
+    }
+});
